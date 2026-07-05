c5d2dd362cfa01867d5f4d2c67668da736e1d221
neron <neronatac1@gmail.com>
2026-07-05 17:39:43 +0200

    implemented compressed ADRS

--- src/pqc/slh_dsa/adrs.rs
+++ src/pqc/slh_dsa/adrs.rs
@@ -2,15 +2,34 @@
 //! See section 4.2 and 4.3
 
 
+pub trait AdrsTrait {
+    type TreeAddrType;
+    type LayerAddressType;
+    type AsBytesType;
+    fn new(layer_addr: Self::LayerAddressType, tree_addr: Self::TreeAddrType, type_: AdrsType) -> Self; // getters
+    fn get_key_pair_address(self) -> u32;
+    fn get_tree_index(self) -> u32;
+    fn set_layer_address(&mut self, layer_addr: Self::LayerAddressType);
+    fn set_tree_address(&mut self, tree_addr: Self::TreeAddrType);
+    fn set_type_and_clear(&mut self, type_: AdrsType);
+    fn set_key_pair_address(&mut self, key_pair_addr: u32);
+    fn set_chain_address(&mut self, chain_addr: u32);
+    fn set_tree_height(&mut self, tree_height: u32);
+    fn set_hash_address(&mut self, hash_address: u32);
+    fn set_tree_index(&mut self, tree_index: u32);
+    fn as_bytes(&self) -> Self::AsBytesType;
+}
+
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum AdrsType {
-    WOTS_HASH = 0,
-    WOTS_PK = 1,
+    WotsHash = 0,
+    WotsPk = 1,
     TREE = 2,
-    FORS_TREE = 3,
-    FORS_ROOTS = 4,
-    WOTS_PRF = 5,
-    FORS_PRF = 6,
+    ForsTree = 3,
+    ForsRoots = 4,
+    WotsPrf = 5,
+    ForsPrf = 6,
 }
 
 pub struct Adrs {
@@ -20,8 +39,19 @@ pub struct Adrs {
     contents: [u8; 12],
 }
 
-impl Adrs {
-    pub fn new(layer_addr: u32, tree_addr: [u8; 12], type_: AdrsType) -> Self {
+pub struct AdrsC { // compressed version of ADRS
+    layer_addr: u8,
+    tree_addr: [u8; 8],
+    type_: AdrsType,
+    contents: [u8; 12],
+}
+
+impl AdrsTrait for Adrs {
+    type TreeAddrType = [u8; 12];
+    type LayerAddressType = u32;
+    type AsBytesType = [u8; 32];
+
+    fn new(layer_addr: Self::LayerAddressType, tree_addr: Self::TreeAddrType, type_: AdrsType) -> Self {
         Self {
             layer_addr,
             tree_addr,
@@ -31,7 +61,7 @@ impl Adrs {
     }
 
     // getters
-    pub fn get_key_pair_address(self) -> u32 {
+    fn get_key_pair_address(self) -> u32 {
         if self.type_ == AdrsType::TREE {
             panic!("Unsupported Adrs type for this operation");
         }
@@ -39,30 +69,30 @@ impl Adrs {
         u32::from_be_bytes(self.contents[0..4].try_into().unwrap())
     }
 
-    pub fn get_tree_index(self) -> u32 {
+    fn get_tree_index(self) -> u32 {
         if self.type_ == AdrsType::TREE
-            || self.type_ == AdrsType::FORS_TREE
-            || self.type_ == AdrsType::FORS_PRF {
+            || self.type_ == AdrsType::ForsTree
+            || self.type_ == AdrsType::ForsPrf {
             return u32::from_be_bytes(self.contents[8..12].try_into().unwrap());
         }
 
         panic!("Unsupported Adrs type for this operation");
     }
 
-    pub fn set_layer_address(&mut self, layer_addr: u32) {
-        self.layer_addr = layer_addr
+    fn set_layer_address(&mut self, layer_addr: Self::LayerAddressType) {
+        self.layer_addr = layer_addr;
     }
 
-    pub fn set_tree_address(&mut self, tree_addr: [u8; 12]) {
-        self.tree_addr = tree_addr
+    fn set_tree_address(&mut self, tree_addr: Self::TreeAddrType) {
+        self.tree_addr = tree_addr;
     }
 
-    pub fn set_type_and_clear(&mut self, type_: AdrsType) {
+    fn set_type_and_clear(&mut self, type_: AdrsType) {
         self.type_ = type_;
         self.contents = [0; 12];
     }
 
-    pub fn set_key_pair_address(&mut self, key_pair_addr: u32) {
+    fn set_key_pair_address(&mut self, key_pair_addr: u32) {
         if self.type_ == AdrsType::TREE {
             panic!("Unsupported Adrs type for this operation");
         }
@@ -70,48 +100,48 @@ impl Adrs {
         self.contents[0..4].copy_from_slice(&key_pair_addr.to_be_bytes());
     }
 
-    pub fn set_chain_address(&mut self, chain_addr: u32) {
-        if self.type_ != AdrsType::WOTS_HASH
-            && self.type_ != AdrsType::WOTS_PRF {
+    fn set_chain_address(&mut self, chain_addr: u32) {
+        if self.type_ != AdrsType::WotsHash
+            && self.type_ != AdrsType::WotsPrf {
             panic!("Unsupported Adrs type for this operation");
         }
 
         self.contents[4..8].copy_from_slice(&chain_addr.to_be_bytes());
     }
 
-    pub fn set_tree_height(&mut self, tree_height: u32) {
-        if self.type_ == AdrsType::FORS_PRF && tree_height != 0 {
+    fn set_tree_height(&mut self, tree_height: u32) {
+        if self.type_ == AdrsType::ForsPrf && tree_height != 0 {
             panic!("tree_height must be 0 in this case")
         } else if self.type_ != AdrsType::TREE
-            && self.type_ != AdrsType::FORS_TREE {
+            && self.type_ != AdrsType::ForsTree {
             panic!("Unsupported Adrs type for this operation");
         }
 
         self.contents[4..8].copy_from_slice(&tree_height.to_be_bytes());
     }
 
-    pub fn set_hash_address(&mut self, hash_address: u32) {
-        if self.type_ == AdrsType::WOTS_PRF && hash_address != 0 {
+    fn set_hash_address(&mut self, hash_address: u32) {
+        if self.type_ == AdrsType::WotsPrf && hash_address != 0 {
             panic!("hash_address must be 0 in this case")
         }
-        else if self.type_ != AdrsType::WOTS_HASH {
+        else if self.type_ != AdrsType::WotsHash {
             panic!("Unsupported Adrs type for this operation");
         }
 
         self.contents[8..12].copy_from_slice(&hash_address.to_be_bytes());
     }
 
-    pub fn set_tree_index(&mut self, tree_index: u32) {
-        if self.type_ == AdrsType::WOTS_PK
-            || self.type_ == AdrsType::WOTS_HASH
-            || self.type_ == AdrsType::FORS_ROOTS
-            || self.type_ == AdrsType::WOTS_PRF {
+    fn set_tree_index(&mut self, tree_index: u32) {
+        if self.type_ == AdrsType::WotsPk
+            || self.type_ == AdrsType::WotsHash
+            || self.type_ == AdrsType::ForsRoots
+            || self.type_ == AdrsType::WotsPrf {
             panic!("Unsupported Adrs type for this operation");
         }
         self.contents[8..12].copy_from_slice(&tree_index.to_be_bytes());
     }
 
-    pub fn as_bytes(&self) -> [u8; 32] {
+    fn as_bytes(&self) -> Self::AsBytesType {
         let mut bytes = [0; 32];
 
         bytes[0..4].copy_from_slice(&self.layer_addr.to_be_bytes());
@@ -119,6 +149,112 @@ impl Adrs {
         let type32 = self.type_ as u32;
         bytes[16..20].copy_from_slice(&type32.to_be_bytes());
         bytes[20..24].copy_from_slice(&self.contents);
+
+        bytes
+    }
+}
+
+impl AdrsTrait for AdrsC {
+    type TreeAddrType = [u8; 8];
+    type LayerAddressType = u8;
+    type AsBytesType = [u8; 22];
+
+    fn new(layer_addr: Self::LayerAddressType, tree_addr: Self::TreeAddrType, type_: AdrsType) -> Self {
+        Self {
+            layer_addr,
+            tree_addr,
+            type_,
+            contents: [0; 12],
+        }
+    }
+
+    fn get_key_pair_address(self) -> u32 {
+        if self.type_ == AdrsType::TREE {
+            panic!("Unsupported Adrs type for this operation");
+        }
+
+        u32::from_be_bytes(self.contents[0..4].try_into().unwrap())
+    }
+
+    fn get_tree_index(self) -> u32 {
+        if self.type_ == AdrsType::TREE
+            || self.type_ == AdrsType::ForsTree
+            || self.type_ == AdrsType::ForsPrf {
+            return u32::from_be_bytes(self.contents[8..12].try_into().unwrap());
+        }
+
+        panic!("Unsupported Adrs type for this operation");
+    }
+
+    fn set_layer_address(&mut self, layer_addr: Self::LayerAddressType) {
+        self.layer_addr = layer_addr;
+    }
+
+    fn set_tree_address(&mut self, tree_addr: Self::TreeAddrType) {
+        self.tree_addr = tree_addr;
+    }
+
+    fn set_type_and_clear(&mut self, type_: AdrsType) {
+        self.type_ = type_;
+        self.contents = [0; 12];
+    }
+
+    fn set_key_pair_address(&mut self, key_pair_addr: u32) {
+        if self.type_ == AdrsType::TREE {
+            panic!("Unsupported Adrs type for this operation");
+        }
+
+        self.contents[0..4].copy_from_slice(&key_pair_addr.to_be_bytes());
+    }
+
+    fn set_chain_address(&mut self, chain_addr: u32) {
+        if self.type_ != AdrsType::WotsHash
+            && self.type_ != AdrsType::WotsPrf {
+            panic!("Unsupported Adrs type for this operation");
+        }
+
+        self.contents[4..8].copy_from_slice(&chain_addr.to_be_bytes());
+    }
+
+    fn set_tree_height(&mut self, tree_height: u32) {
+        if self.type_ == AdrsType::ForsPrf && tree_height != 0 {
+            panic!("tree_height must be 0 in this case")
+        } else if self.type_ != AdrsType::TREE
+            && self.type_ != AdrsType::ForsTree {
+            panic!("Unsupported Adrs type for this operation");
+        }
+
+        self.contents[4..8].copy_from_slice(&tree_height.to_be_bytes());
+    }
+
+    fn set_hash_address(&mut self, hash_address: u32) {
+        if self.type_ == AdrsType::WotsPrf && hash_address != 0 {
+            panic!("hash_address must be 0 in this case")
+        }
+        else if self.type_ != AdrsType::WotsHash {
+            panic!("Unsupported Adrs type for this operation");
+        }
+
+        self.contents[8..12].copy_from_slice(&hash_address.to_be_bytes());
+    }
+
+    fn set_tree_index(&mut self, tree_index: u32) {
+        if self.type_ == AdrsType::WotsPk
+            || self.type_ == AdrsType::WotsHash
+            || self.type_ == AdrsType::ForsRoots
+            || self.type_ == AdrsType::WotsPrf {
+            panic!("Unsupported Adrs type for this operation");
+        }
+        self.contents[8..12].copy_from_slice(&tree_index.to_be_bytes());
+    }
+
+    fn as_bytes(&self) -> Self::AsBytesType {
+        let mut bytes = [0; 22];
+
+        bytes[0] = self.layer_addr;
+        bytes[1..9].copy_from_slice(&self.tree_addr);
+        bytes[9] = self.type_ as u8;
+        bytes[10..22].copy_from_slice(&self.contents);
 
         bytes
     }
--- src/pqc/slh_dsa/wotsplus.rs
+++ src/pqc/slh_dsa/wotsplus.rs
@@ -1,24 +1,29 @@
-use crate::pqc::slh_dsa::adrs::Adrs;
+use std::marker::PhantomData;
+use crate::pqc::slh_dsa::adrs::{AdrsTrait};
 
-pub struct WOTSPlus<const N: usize, F>
+pub struct WOTSPlus<const N: usize, F, ADRS>
 where
-    F: Fn(&[u8; N], &[u8; 32], &[u8; N]) -> [u8; N]
+    F: Fn(&[u8; N], &ADRS::AsBytesType, &[u8; N]) -> [u8; N],
+    ADRS: AdrsTrait
 {
     f: F,
+    _adrs: PhantomData<ADRS>, // PhantomData to fakely use ADRS type constraint
 }
 
-impl<const N: usize, F> WOTSPlus<N, F>
+impl<const N: usize, F, ADRS> WOTSPlus<N, F, ADRS>
 where
-    F: Fn(&[u8; N], &[u8; 32], &[u8; N]) -> [u8; N]
+    F: Fn(&[u8; N], &ADRS::AsBytesType, &[u8; N]) -> [u8; N],
+    ADRS: AdrsTrait
 {
     pub fn new(f: F) -> Self {
-        WOTSPlus{
-            f
+        WOTSPlus::<N, F, ADRS>{
+            f,
+            _adrs: PhantomData,
         }
     }
 
-    fn chain(&self, x: &[u8; N], i: usize, s: usize, seed: &[u8; N], adrs: &mut Adrs) -> [u8; N] {
-        let tmp = x.clone();
+    fn chain(&self, x: &[u8; N], i: usize, s: usize, seed: &[u8; N], adrs: &mut ADRS) -> [u8; N] {
+        let tmp = *x; // copy
         for j in i..i + s {
             adrs.set_hash_address(j as u32);
             (self.f)(seed, &adrs.as_bytes(), &tmp);
