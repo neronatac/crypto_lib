378c7766ff0e62f82e3012bbf791672b4ce5f171
neron <neronatac1@gmail.com>
2026-07-31 08:41:22 +0200

    work in progress, need MGF1

--- src/pqc/slh_dsa/mod.rs
+++ src/pqc/slh_dsa/mod.rs
@@ -1,6 +1,10 @@
 //! Stateless Hash-based Digital Signature Algorithm
 //! Cf. FIPS 205
 
+use crate::hash::common::Hash;
+use crate::hash::sha::SHA256;
+use crate::pqc::slh_dsa::slh_dsa::SLHDSA;
+
 mod adrs;
 mod fors;
 mod hypertree;
@@ -8,3 +12,27 @@ mod slh_dsa;
 mod utils;
 mod wotsplus;
 mod xmss;
+
+
+// variants definitions
+pub type SlhDsa128s<ADRS, F, PRF, T, HFUNC, PRFMSG, HMSG> =
+    SLHDSA<16, 7, 9, 12, 14, 63, ADRS, F, PRF, T, HFUNC, PRFMSG, HMSG>;
+
+pub type SlhDsaSha2_128s = SlhDsa128s<AdrsC, >;
+
+// SHA2 - Security Category 1
+fn hmsg_sha2_cat1<const M: usize>(r: &[u8; 16], pk_seed: &[u8; 16], pk_root: &[u8; 16], m: &[u8]) -> Vec<u8> {
+    let mut hash = SHA256::new(&());
+    hash.update(r);
+    hash.update(pk_seed);
+    hash.update(pk_root);
+    hash.update(m);
+    let tmp = hash.finalise();
+
+    let mut concat = Vec::with_capacity(16 + 16 + 32);
+    concat.extend_from_slice(r);
+    concat.extend_from_slice(pk_seed);
+    concat.extend_from_slice(&tmp);
+
+    mgf1<SHA256>(concat, M)
+}
